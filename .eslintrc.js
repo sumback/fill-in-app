3a51548db6ef422f05169102d3d5d58ae9b00875
Sumback <pro.jp.ferreira@gmail.com>
2021-12-16 22:16:14 +0100

    config(app) : add eslint and prettier

---
+++ .eslintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+  env: {
+    node: true,
+    'vue/setup-compiler-macros': true,
+  },
+  extends: ['eslint:recommended', 'plugin:vue/vue3-recommended', 'prettier'],
+  rules: {
+    'vetur.validation.template': 0,
+    'vue/require-default-prop': 'off',
+    quotes: [
+      'warn',
+      'single',
+      { avoidEscape: true, allowTemplateLiterals: false },
+    ],
+  },
+};
